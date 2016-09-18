7c8cfd45a8a2f0dde7af0d589242bf2354a84629
Ross Byrne <ross.byrne@outlook.ie>
2016-09-18 16:33:48 +0100

    Added For Loop Example

---
+++ ForLoops/forloops.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    fmt.Println("\nFor Loop example\n")
+    fmt.Println("Loops 10 times.\n");
+    
+    for i := 0; i < 10; i++ {
+        
+        fmt.Println(i)
+        
+    } // for
+    
+} // main()
